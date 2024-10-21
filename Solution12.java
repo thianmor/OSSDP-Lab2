182fd7ee3edf29d6089374626cf24aa731665be5
thianmor <115472400+thianmor@users.noreply.github.com>
2024-10-21 20:55:57 +0800

    Update Solution12.java
    test

--- Solution12.java
+++ Solution12.java
@@ -62,3 +62,31 @@ class Solution {
     }
 }
 
+package com.example.lab2codes;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class L2022211968_12_Test {
+    @Test
+    public void testMultiply_ValidInputs() {
+        Solution solution = new Solution();
+        assertEquals("6", solution.multiply("2", "3"));
+        assertEquals("56088", solution.multiply("123", "456"));
+    }
+
+    @Test
+    public void testMultiply_ZeroInput() {
+        Solution solution = new Solution();
+        assertEquals("0", solution.multiply("0", "5"));
+        assertEquals("0", solution.multiply("5", "0"));
+        assertEquals("0", solution.multiply("0", "0"));
+    }
+
+    @Test
+    public void testMultiply_LargeNumbers() {
+        Solution solution = new Solution();
+        assertEquals("1000000", solution.multiply("1000", "1000"));
+    }
+}
